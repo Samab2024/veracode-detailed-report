0aa0639e2c5dbd0ee35a50ea7b3ea77ecefb7b36
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2025-10-20 18:49:10 +0530

    Initial commit

--- examples/run_report_example.py
+++ examples/run_report_example.py
@@ -2,6 +2,6 @@ from veracode_report.get_detailed_report import get_build_id, fetch_detailed_rep
 
 if __name__ == "__main__":
     app_id = "2223648"
-    report_type = "XML"  # or "PDF" 
+    report_type = "XML"  # or "PDF"
     build_id = get_build_id(app_id)
     fetch_detailed_report(build_id, report_type)
