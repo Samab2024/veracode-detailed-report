ba22b3d4edd0220ada2cfa11928a634b9c5623ff
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2025-10-29 18:03:50 +0530

    Update app_info.py

--- veracode_xml/tasks/app_info.py
+++ veracode_xml/tasks/app_info.py
@@ -1,4 +1,4 @@
-from ..utils.api_helpers import get_veracode_session, resolve_app_id
+from veracode_xml.utils.api_helpers import get_veracode_session, resolve_app_id
 
 HELP_TEXT = "Fetch detailed information for a specific application."
 
