6ee8a31fff60d77cae04792a8555e9b22835918d
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2025-10-29 16:43:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="veracode-detailed-report",
-    version="1.1.0",
-    description="Fetch Veracode detailed reports (XML or PDF) using HMAC authentication.",
+    name="veracode-xml",
+    version="1.0.0",
+    description="Unified CLI for Veracode XML API tasks like fetching detailed reports.",
     author="Soumik Roy",
     packages=find_packages(),
     install_requires=[
         "requests",
-        "veracode-api-signing"
+        "veracode-api-signing",
     ],
     entry_points={
         "console_scripts": [
-            "veracode-report=veracode_report.get_detailed_report:main",
+            "veracode-xml=veracode_xml.cli:main",
         ],
     },
     python_requires=">=3.8",
