9ad69a9ff640012ec7eb0fdc0ec8d5ce3a0ad5f5
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2025-10-29 22:00:50 +0530

    Update build_list.py

--- veracode_xml/tasks/build_list.py
+++ veracode_xml/tasks/build_list.py
@@ -1,4 +1,4 @@
-from veracode_xml.utils.api_helpers import get_veracode_session, resolve_app_id
+from ..utils.api_helpers import get_veracode_session, resolve_app_id
 
 HELP_TEXT = "List all builds for a specific application."
 
